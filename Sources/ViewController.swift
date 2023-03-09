303d9de2e3ee7760d524073a1a973edf89312e00
CastleSilver <soulll012@naver.com>
2023-03-09 10:29:58 +0900

    [refactor] lottie animation을 UIView에서 띄우기 - 기존에 코드로 호출하고 크기를 지정했던 것을 UIView로 관리 - 코드의 길이가 짧아지고 기종 별로 동일한 UI를 안정적으로 제공 가능

--- Sources/ViewController.swift
+++ Sources/ViewController.swift
@@ -10,19 +10,12 @@ import Lottie
 
 class ViewController: UIViewController {
     @IBOutlet weak var logoImage: UIImageView!
-    private var animationView: LottieAnimationView?
+    @IBOutlet weak var animationView: LottieAnimationView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // add lottie animation
-        animationView = .init(name: "splash-image")
-        let screenBounds = UIScreen.main.bounds
-        // auto layout 사용해서 지정해도 됨
-        animationView!.frame = CGRect(
-                                      x: -(screenBounds.width * 0.05), y: screenBounds.height * 0.4,
-                                      width: screenBounds.width * 1.1, height: screenBounds.height * 0.6
-                               )
-        animationView!.contentMode = .scaleAspectFit
+        animationView!.contentMode = .scaleAspectFill
         animationView!.loopMode = .loop
         self.view.addSubview(animationView!)
         animationView!.play()
