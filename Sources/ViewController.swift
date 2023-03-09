3824eb44cc68d2918942955a08716efd0891d082
CastleSilver <soulll012@naver.com>
2023-03-09 10:21:32 +0900

    [feat] splash view 화면에 lottie animation 추가 - lottie pod install - storyboard 상 별도의 view 생성 없이 삽입 - 코드를 이용해서 크기 지정

--- Sources/ViewController.swift
+++ Sources/ViewController.swift
@@ -6,12 +6,26 @@
 //
 
 import UIKit
+import Lottie
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var logoImage: UIImageView!
+    private var animationView: LottieAnimationView?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // add lottie animation
+        animationView = .init(name: "splash-image")
+        let screenBounds = UIScreen.main.bounds
+        // auto layout 사용해서 지정해도 됨
+        animationView!.frame = CGRect(
+                                      x: -(screenBounds.width * 0.05), y: screenBounds.height * 0.4,
+                                      width: screenBounds.width * 1.1, height: screenBounds.height * 0.6
+                               )
+        animationView!.contentMode = .scaleAspectFit
+        animationView!.loopMode = .loop
+        self.view.addSubview(animationView!)
+        animationView!.play()
     }
 
 
